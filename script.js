865fef92accbfc301c87b4a8ed7a4ebebb51e7bc
Serhii Vinokhodov <vinokhodova.nadin@gmail.com>
2022-12-15 20:31:40 +0100

    add task

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
 let marvelHeroes = [
-	{
-		name: "Thor"
-	},
-	{
-		name: "Spider Man"
-	}
+    {
+        name: "Thor"
+    },
+    {
+        name: "Spider Man"
+    }
 ];
 
 let dcHeroes = [
-	{
-		name: "Superman"
-	},
-	{
-		name: "Batman"
-	},
-	{
-		name: "Deadpool"
-	}
+    {
+        name: "Superman"
+    },
+    {
+        name: "Batman"
+    },
+    {
+        name: "Deadpool"
+    }
 ];
+
+
+Array.prototype.heroesRender = function (folder) {
+    const newArr = this
+        .map(function (character) {
+            return `
+            <tr>
+                <td>${character.name}</td>
+                <td><img src="images/${folder}/${character.name.toLowerCase().replace(" ", "")}.svg" alt="${character.name}" width="40"></td>
+            </tr>
+        `
+        })
+        .join("");
+
+    document.write(`
+		<table>
+			<thead>
+				<tr>
+					<th>Image</th>
+					<th>Name</th>
+				</tr>
+			</thead>
+			<tbody>${newArr}</tbody>
+		</table>
+	`)
+}
+
+marvelHeroes.heroesRender('marvel');
+dcHeroes.heroesRender('dc');
+
+
+
 
